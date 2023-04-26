c632b529ba4cd2cdcddcfd7dc1f2424a6e8b33c3
Kherin <kherin.j.s.bundhoo@gmail.com>
2023-04-26 22:25:35 +0400

    dynamically capturing the height and width of the inner browser window

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [widthInEmp, setWidthInEm] = useState(0);
+  const [heightInEmp, setHeightInEmp] = useState(0);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const emSize = parseFloat(
+        getComputedStyle(document.documentElement).fontSize
+      );
+
+      setWidthInEm(window.innerWidth / emSize);
+      setHeightInEmp(window.innerHeight / emSize);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      Width in em: {widthInEmp}, Height in em: {heightInEmp}
+    </div>
+  );
 }
-
-export default App
+export default App;
